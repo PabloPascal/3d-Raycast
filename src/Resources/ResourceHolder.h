8511e42a4fe05f94607eddebeae082324341dd72
PabloPascal <zycovandreypetrovich@gmail.com>
2025-09-15 17:47:42 +0800

    removed old code class ResourceHolder now i use only ResourceManager

--- src/Resources/ResourceHolder.h
+++ src/Resources/ResourceHolder.h
@@ -16,59 +16,6 @@
 
 
 
-template <typename Identifier,typename Resource>
-class ResourceHolder {
-
-	std::unordered_map<Identifier, std::unique_ptr<Resource>> mResourceMap;
-
-public:
-
-	void load(Identifier id,const std::string& filename);
-
-	Resource& get(Identifier id);
-
-	const Resource& get(Identifier id) const;
-	
-};
-
-template <typename Identifier, typename Resource>
-void ResourceHolder<Identifier, Resource>::load(Identifier id,const std::string& filename){
-	std::unique_ptr<Resource> res = std::make_unique<Resource>(Resource());
-	
-	if (!res->loadFromFile(filename)) {
-		throw std::runtime_error("ResourceHolder::load can't load file" + filename);
-	}
-
-	auto inserted = mResourceMap.insert(std::make_pair(id, std::move(res)));
-	assert(inserted.second);
-
-}
-
-
-template <typename Identifier, typename Resource>
-Resource& ResourceHolder<Identifier, Resource>::get(Identifier id) {
-
-	auto it = mResourceMap.find(id);
-
-	//assert(it != mResourceMap.end());
-
-	return (*it->second);
-}
-
-
-template <typename Identifier, typename Resource>
-const Resource& ResourceHolder<Identifier, Resource>::get(Identifier id) const
-{
-
-	auto it = mResourceMap.find(id);
-
-	assert(it != mResourceMap.end());
-
-	return (*it->second);
-}
-
-
-
 
 class ResourceManager
 {
